8e51fb8a84d16c2823a939f8e067ffce9c6c709c
Ryan Xin <xinpf0715@gmail.com>
2020-09-20 00:05:05 +1000

    login route

--- models/Admin.js
+++ models/Admin.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema({
-  // _id: mongoose.Schema.Types.ObjectId,
   name: String,
   email: String,
   password: String,
--- server.js
+++ server.js
@@ -1,27 +1,34 @@
 /* ------------------ DB Initialization ----------------- */
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/covid19', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Mongoose connection error:'));
-db.once("open", function () {
-  console.log('Mongoose connected');
-}); // Mongoose connection
+const db = require('./models');
+
+
+mongoose
+.connect('mongodb://127.0.0.1:27017/covid19', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Successfully connect to MongoDB.'))
+.catch(err => console.error('Connection error', err));
+
+console.log(db.User.find());
+// const db = mongoose.connection;
 
 /* ------------ Express Server Initialization ----------- */
 
 const express = require('express');
-const server = express();
+const app = express();
 const PORT = 1337;
 
 // Enable support for JSON-encoded request bodies(i.e. posted form data)
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Use cors as Express middleware, i.e. se the CORS allow header
 const cors = require('cors');
-server.use(cors());
+app.use(cors());
 
 // For checking password when login
 const bcrypt = require('bcrypt');
@@ -35,6 +42,81 @@ const jwtAuthenticate = require('express-jwt');
 // TODO: Move this out of server file, into .env or .bash_profile etc
 const SERVER_SECRET_KEY = 'covid19TrackerSecretKey';
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
+
+// Use this function to lock down any route that should be for logged-in users only
+const checkAuth = () => {
+  // Check authentication for this route, i.e. logged-in users only
+  return jwtAuthenticate({
+    secret: SERVER_SECRET_KEY,
+    algorithms: ['HS256']
+  });
+};
+
+/* ----------------------- Routes ----------------------- */
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to covid-19 tracker.' });
+});
+
+app.post('/user/login', (req, res) => {
+  console.log('posted data', req.body);
+  // res.json(req.body); // echo back the POSTed form data
+  const {
+    email,
+    password
+  } = req.body; // const email = req.body.email
+  db.User.findOne({
+    email
+  }, (err, user) => { // {email: email}
+    if (err) {
+      res.status(500).json({
+        message: 'Server error'
+      })
+      return console.log('Error retrieving user', err);
+    }
+    console.log('User found', user);
+    // res.json(user);
+    // Check that we actually found a user with the specified email, and also that the password given matches the password for that user
+    if (user && bcrypt.compareSync(password, user.password)) {
+      // Successful login
+      // Generate a signed JWT token which contains the user data
+      const token = jwt.sign({
+          _id: user._id,
+          email: user.email,
+          name: user.name
+        },
+        SERVER_SECRET_KEY, {
+          expiresIn: '72h'
+        }
+      );
+      res.json({
+        user,
+        token,
+        success: true
+      });
+    } else {
+      // Either user not found or password doesn't match
+      res.status(401).json({
+        message: 'Invalid email or password.'
+      });
+    }
+  }); // findOne
+}); // post user/login
+
+
+
+
+
+
+// Define an error handler function for express to use whenever there is an authentication error
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    console.log('Unauthorized Request', req.path);
+    res.status(401).json({
+      error: 'Invalid token'
+    });
+  }
+}); // error handler
